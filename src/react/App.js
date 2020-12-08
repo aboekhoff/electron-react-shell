1bd7f0c66c323963d6e5feba40abc5b15852d5b0
Andrew Boekhoff <boekhoffa@gmail.com>
2020-12-08 13:14:37 -0800

    Initial electron-react-app shell

--- src/react/App.js
+++ src/react/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { channels } from '../shared/constants';
+const { ipcRenderer } = window;
+
 function App() {
   return (
     <div className="App">
